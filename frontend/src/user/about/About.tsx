c727c848f854a54c0a696f45bd7b51e0573321e9
cedwics10 <cedric.von@outlook.fr>
2025-02-12 10:27:37 +0100

    First route are succesfull*y created

---
+++ frontend/src/user/about/About.tsx
@@ -0,0 +1,12 @@
+/* import { useState } from 'react'
+import viteLogo from '/vite.svg' 
+
+import '../App.css' */
+
+function About() {
+  return (
+    <h1>En construction !</h1>
+  );
+}
+
+export default About;
---
+++ frontend/src/user/index/Index.tsx
@@ -0,0 +1,12 @@
+/* import { useState } from 'react'
+import viteLogo from '/vite.svg' 
+
+import '../App.css' */
+
+function Index() {
+  return (
+    <h1>Bienvenu</h1>
+  );
+}
+
+export default Index;
