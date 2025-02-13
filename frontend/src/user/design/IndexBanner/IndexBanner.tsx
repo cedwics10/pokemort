44f72765f9f1f9a864fa6da94ebfc0b8dd5008b1
cedwics10 <cedric.von@outlook.fr>
2025-02-13 17:19:33 +0100

    React - front - index  vbanner created

---
+++ frontend/src/user/design/IndexBanner/IndexBanner.tsx
@@ -0,0 +1,13 @@
+import BannerImage from '../images/banner-image.jpg';
+
+import MainBanner from '../MainBanner/MainBanner';
+
+function IndexBanner() {
+  return (<>
+      <MainBanner>
+      </MainBanner >
+    </>
+  );
+}
+
+export default IndexBanner;
